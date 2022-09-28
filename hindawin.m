40eede0f89762ad383284834709b604aab174c33
ManuelOrtizI <72362839+ManuelOrtizI@users.noreply.github.com>
2022-09-27 19:56:46 -0500

    Pase el algoritmo a hindawin.

--- hindawin.m
+++ hindawin.m
@@ -3,16 +3,16 @@ close all
 
 %Declaración de Variables
 
-N=[24 36 48 60 72 84 96];% Número de ventanas
-teta=[2 3 4 5 6 7 8 9 10].^-3;%Tasa de abandono general
+N=[96 84 72 60 48 36 24];% Número de ventanas
+teta=[2 4 6 8 10].^-3;%Tasa de abandono general
 lmb=0.04;%Tasa de arribos 
 c=0.00407;%Tasa de descarga general
-mu= 0.00255;%Tasa de subida general
+mu=0.00255;%Tasa de subida general
 gamma=0.006;%Tasa de abandono general de los seeds
-w_prom=zeros(length(N),length(teta));%Cadena acumulativa para obtener el promedio por iteración 
-y_prom=zeros(length(N),length(teta));%Vector de seeds promedio
-T_prom=zeros(length(N),length(teta));%Vector de tiempos promedio
-IT=100;
+X_prom=zeros(length(N),length(teta));%Matriz de downloaders promedio
+Y_prom=zeros(length(N),length(teta));%Matriz de seeds promedio
+IT=10000;
+
 for idxn=1:length(N)
         
    for idxt=1:length(teta) 
@@ -21,126 +21,124 @@ for idxn=1:length(N)
       mw=N(idxn)*mu;%Tasa de subida máxima
       W=zeros(1,N(idxn)+1);%Vector de poblaciones por ventana de video
       W(N(idxn)+1)=1;%Estado Inicial (0,0,0,0,...,1)
-      tp=zeros(1,N(idxn)+1);%Vector de tiempos promedio por ventana
-      wi=zeros(1,N(idxn)+1);%Vector de poblaciones para generar V.A.
-      xi_prom=0;%Vector de poblaciones (downloaders) promedio
-      t_prom=zeros(1,N(idxn)+1);%Vector de tiempo promedio por iteración
+      wi_prom=0;%Cadena acumulativa para obtener el promedio por iteración 
+      yi_prom=0;%Vector de seeds promedio
+      tp=0;%Vector de tiempos promedio por ventana
       TAb=zeros(1,N(idxn)+1);%Vector para tasa de abandono
+      tab=zeros(1,N(idxn)+1);%Vector para tasa de abandono
       tao_mw=zeros(1,N(idxn));%Vector para tasa de abandono
+      TTran=zeros(1,N(idxn));%Vector para tasa de abandono
             
-      for iter=1:IT             
-                
+      for iter=1:IT
+          
           %Caso estado (0,0,0,....,1)
           if W(1:N(idxn))==0%Arribo porque las poblaciones de 0 a N son 0 
-
              W(1)=W(1)+1;
-             tp=tp+exprnd(lmb);
-             
+             tp=tp+exprnd(1/lmb);      
           %Caso estado ~= (0,0,0,....,1)
-          else %Determinar evento porque las poblaciones de 0 a N son ~=0
-             
-             %Generar tasas de arribo y abandono por ventana
-             TArr=1/lmb;%Tasa promedio de arribo a la ventana 0
-             TAb=teta(idxt)*W(1:N(idxn));%Tasas promedio de abando de la ventana 0 a N-1
+          else%Determinar evento porque las poblaciones de 0 a N son ~=0
+              
+              %Generar tasas de arribo y abandono por ventana
+              TArr=1/lmb;%Tasa promedio de arribo a la ventana 0
+              TAb=teta(idxt)*W(1:N(idxn));
 
-             %Tasa de abandono para seeds
-             if 2<= W(N(idxn)+1)
-                TAb(N(idxn)+1)=gamma*(W(N(idxn)+1)-1);
-             else
-                TAb(N(idxn)+1)=inf;
-             end
+              %Abandono para seeds
+              Si=W(N(idxn)+1);
+              if Si >= 2
+                 TAb(N(idxn)+1)=gamma*(Si-1);
+              else
+                 TAb(N(idxn)+1)=inf;
+              end
 
-             %Tranferencia para usuarios en ventanas 0 a N-2        
-             tao_cw=cw*W(1:N(idxn)-1);%Tasa promedio de descarga en abundancia
-             
-             for i=1:N(idxn)-1
-                 for k=i+1:N(idxn)
-                     tao_mw(i)=tao_mw(i)+(mw*W(i)*(W(k)/sum(W(1:k-1))));%Tasa promedio de descarga en penuria
-                 end
-                 tao_mw(i)=tao_mw(i)+(W(N(idxn)+1)/sum(W(1:N(idxn))));
-             end
+              %Tranferencia para usuarios en ventanas 0 a N-2        
+              tao_cw=cw*W(1:N(idxn));%Tasa promedio de descarga en abundancia
 
-             %Transferencia para usuarios en ventana N-1
-             tao_cw(N(idxn))=cw*W(N(idxn));%Tasa promedio de descarga en abundancia        
-             tao_mw(N(idxn))=mw*W(N(idxn))*(W(N(idxn)+1)/sum(W(1:N(idxn))));%Tasa promedio de descarga en penuria
+              for i=1:N(idxn)-1
+                  for k=i+1:N(idxn)
+                         tao_mw(i)=tao_mw(i)+(mw*W(i)*(W(k)/sum(W(1:k-1))));%Tasa promedio de descarga en penuria
+                  end
+                  tao_mw(i)=tao_mw(i)+(W(N(idxn)+1)/sum(W(1:N(idxn))));
+              end
 
-             %Tiempos infinitos para descartar un abandono o transferencia
-             %inválida
-             
-             for idxab=1:N(idxn)+1%Cambir el vector a infinitos para descartar poblaciones en 0
-                if(TAb(idxab)==0)
-                    tab(idxab)=inf;
-                else
-                    tab(idxab)=TAb(idxab);
-                end
-             end
-             
-             for idxtao=1:N(idxn)%Cambir el vector a infinitos para descartar poblaciones en 0
-                if(tao_cw(idxtao)==0)
-                    Rtao_cw(idxtao)=inf;
-                else
-                    Rtao_cw(idxtao)=tao_cw(idxtao);
-                end
-             end
-             
-             for idxtao=1:N(idxn)%Cambir el vector a infinitos para descartar poblaciones en 0
-                if(tao_mw(idxtao)==0)
-                    Rtao_mw(idxtao)=inf;
-                else
-                    Rtao_mw(idxtao)=tao_mw(idxtao);
-                end
-             end
-             
-             %Obtener V.A con las tasas de arribo, abandono y transferencia
-             tao_min=min(Rtao_cw,Rtao_mw);%Tasa promedio de descarga en la ventana i
+              %Tranferencia para usuarios en ventana N-1      
+              tao_mw(N(idxn))=mw*W(N(idxn))*(W(N(idxn)+1)/sum(W(1:N(idxn)-1)));%Tasa promedio de descarga en penuria
 
-             VEArr=exprnd(TArr);%V.A para arribos
-             ab=exprnd(1./TAb);%Vector de V.As para abandonos
-             VEAb=min(ab);% Obtener minimo de ab
-             tran=exprnd(1./tao_min);%Vector de V.As para transferencias
-             VETao=min(tran);% Obtener minimo de tran
+              %Obtener V.A con las tasas de arribo, abandono y transferencia
+              tao_min=min(tao_cw,tao_mw);%Tasa promedio de descarga en la ventana i
 
-             %Obtener el evento que ocurrio
-             Evsucces=min(VEArr,VEAb);
-             Evfinal=min(Evsucces,VETao);
+              VEArr=exprnd(TArr);%V.A para arribos
+              ab=exprnd(1./TAb);%Vector de V.As para abandonos
+              tran=exprnd(1./tao_min);%Vector de V.As para transferencias
+
+              %Tiempos infinitos para descartar un abandono o transferencia
+              %inválida             
+              for idxab=1:N(idxn)+1%Cambir el vector a infinitos para descartar poblaciones en 0
+                  if(ab(idxab)==0)
+                     tab(idxab)=inf;
+                  else
+                     tab(idxab)=ab(idxab);
+                  end
+              end
+              VEAb=min(tab);% Obtener minimo de ab
+
+              for idxtao=1:N(idxn)%Cambir el vector a infinitos para descartar poblaciones en 0
+                  if(tran(idxtao)==0)
+                     TTran(idxtao)=inf;
+                  else
+                     TTran(idxtao)=tran(idxtao);
+                  end
+              end
+              VETao=min(TTran);% Obtener minimo de tran
+
+              %Obtener el evento que ocurrio
+              Evsucces=min(VEArr,VEAb);
+              Evfinal=min(Evsucces,VETao);
 
              %Incrementar o decrementar la población de una ventana dependiendo 
              %del evento ocurrido y de la ventana en donde ocurrio
+
              if Evfinal==VEAb
+
                 idx=find(ab==VEAb);%Encontrar indice donde ab == VEAb
                 W(idx)=W(idx)-1;%Decrementar W en idx
                 tp=tp+VEAb;%Se suma el tiempo promedio a tp en idx
+
              elseif Evfinal==VEArr
+
                 W(1)=W(1)+1;%Incrementar W en 1
                 tp=tp+VEArr;%Se suma el tiempo promedio a tp en 1
+
              elseif Evfinal==VETao
                 idx=find(tran==VETao);%Encontrar indice donde ab == VEAb
                 W(idx)=W(idx)-1;%Decrementar W en idx
                 W(idx+1)=W(idx+1)+1;%Incrementar W en idx+1
                 tp=tp+VETao;%Se suma el tiempo promedio a tp en idx
-             end  
-             %Acumular las poblaciones de downloaders y seeds por cada
-             %iteración
-             xi_prom=xi_prom+(sum(W(1:N(idxn)))*Evfinal);
-             t_prom=t_prom+tp;
-          end          
+             end 
+                wi_prom=wi_prom+(sum(W(1:N(idxn)))*Evfinal);
+                yi_prom=yi_prom+(W(N(idxn)+1)*Evfinal);
+          end
+          w_prom=wi_prom/tp;
+          y_prom=yi_prom/tp;          
       end
          %Obtener los promedios de seeds y downloaders para distintos
          %valores  de N y teta 
-         x_prom(idxn,idxt)=xi_prom/sum(t_prom);
-         T_prom(idxn,idxt)=mean(t_prom);
+         X_prom(idxn,idxt)=w_prom;
+         Y_prom(idxn,idxt)=y_prom;
    end        
 end
      
 figure(1)
-surf(teta,N,x_prom,'FaceAlpha',0.5)
-yticks([24:12:96])
-xlabel('x10^{-3}           \theta')
-ylabel('N')
+surf(N,teta,transpose(X_prom),'FaceAlpha',0.5)
+xticks([24:12:96])
+ylabel('x10^{-3}           \theta')
+xlabel('N')
 zlabel('x')
 title('Número de leeches en equilibrio')
 
 figure(2)
-surf(teta,N,T_prom,'FaceAlpha',0.5)
-yticks([24:12:96])
-xlim([min(teta) max(teta)])
+surf(N,teta,transpose(Y_prom),'FaceAlpha',0.5)
+xticks([24:12:96])
+ylabel('x10^{-3}           \theta')
+xlabel('N')
+zlabel('y')
+title('Número de seeds en equilibrio')
