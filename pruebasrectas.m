750fc5a7d1e977e7e232d9a7bec7efbbbbd0f9b2
ManuelOrtizI <72362839+ManuelOrtizI@users.noreply.github.com>
2022-12-19 20:31:29 -0600

    pruebas

---
+++ pruebasrectas.m
@@ -0,0 +1,20 @@
+clear all
+close all
+
+x=48;
+a=5;
+k=x/2;
+m1=-a/k;
+m2=a/k;
+
+for i=1:x+1
+    if i<=k
+        y(i)=((i-1)*m1)+a;
+    elseif i==k+1
+        y(i)=0;
+    else
+        y(i)=((i-1)-k)*m2;
+    end
+end
+
+plot([0:x],y,'m --','Linewidth',2);
