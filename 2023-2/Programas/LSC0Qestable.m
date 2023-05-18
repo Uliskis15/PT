87e654ce45f3343f677e3ef84b0405f4e3f08ba3
ManuelOrtizI <72362839+ManuelOrtizI@users.noreply.github.com>
2023-05-18 13:12:18 -0600

    agregando 3 reporte parcialagregando 3 reporte parcial

--- 2023-2/Programas/LSC0Qestable.m
+++ 2023-2/Programas/LSC0Qestable.m
@@ -4,7 +4,7 @@ close all
 %Declaración de Variables
 
 C=[36];% Número de ventanas
-Q=12;
+Q=36;
 teta=[2].*(10^-3);%Tasa de abandono general
 lmb=0.04;%Tasa de arribos 
 c=0.00407;%Tasa de descarga general
